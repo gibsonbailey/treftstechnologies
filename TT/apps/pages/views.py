248133795b7254c3ecd702615ab6fdffad7f9af7
gibsonbailey <gibson.bailey@live.com>
2019-07-05 17:09:31 -0700

    Login is functional through rest_auth.

--- TT/apps/pages/views.py
+++ TT/apps/pages/views.py
@@ -7,8 +7,6 @@ class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
     def get_context_data(self, **kwargs):
-
-
         registration_form = TTUserCreationForm()
 
         data = super().get_context_data(**kwargs)
