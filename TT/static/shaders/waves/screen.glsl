1dd4c4f079cb065d12650ba78470e949fb8d838b
gibsonbailey <gibson.bailey@live.com>
2019-09-30 10:33:20 -0700

    Increased brightness.

--- TT/static/shaders/waves/screen.glsl
+++ TT/static/shaders/waves/screen.glsl
@@ -12,8 +12,8 @@ void main()
     float simulation_value = decode(texture2D(simulation_texture, v_tex_coord));
     float boundary_condition_value = decode(texture2D(boundary_conditions, v_tex_coord));
 
-    float pixel_color = max(boundary_condition_value, simulation_value);
-    gl_FragColor = vec4(0.8 * pixel_color, 0.8 * pixel_color, pixel_color, 1.0);
+    float pixel_color = 2.0 * max(boundary_condition_value, simulation_value);
+    gl_FragColor = vec4(0.6 * pixel_color, 0.8 * pixel_color, pixel_color, 1.0);
 }
 
 float decode(vec4 rgba)
