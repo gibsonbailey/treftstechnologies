fa0c03c39f450ee6a2aa302362ee102d8f3e25a6
Bailey Lind-Trefts <gibson.bailey@live.com>
2019-12-22 17:39:05 -0800

    Enabled email in settings. Pulls in host email user and password from environment. Added front end functionality for contact form. Could use better error messages on contact form after validation. Semantic UI is not displaying errors for some reason.

--- TT/settings/base.py
+++ TT/settings/base.py
@@ -19,7 +19,15 @@ ACCOUNT_LOGOUT_ON_GET = True
 
 AUTH_USER_MODEL = 'profiles.TTUser'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 SITE_ID = 3
 
